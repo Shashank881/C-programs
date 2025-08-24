989c154b74f3ffb000b33b1f3782f04b054924b9
Shashank Lodia <lodiashashank@gmail.com>
2025-08-24 19:24:41 +0530

    Program for sum of two values

---
+++ sum.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int a,b,c;
+    printf("Enter two values for sum");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("Sum is %d",c);
+    
+}
