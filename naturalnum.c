88db11a766b91b4ffd3665c35c5c5af710570722
Shashank Lodia <lodiashashank@gmail.com>
2025-08-28 19:50:19 +0530

    Printing natural numbers with while loop.

---
+++ naturalnum.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int n,i;
+    printf("Enter a value ");
+    scanf("%d",&n);
+    i=1;
+    while(i<=n)
+    {
+        printf("%d",i);
+        i++;
+    }
+}
