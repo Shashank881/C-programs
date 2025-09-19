2b48cff43e8680ade876c05c887cf17714dcd046
Shashank Lodia <lodiashashank@gmail.com>
2025-09-19 09:38:10 +0530

    Program to access data in array with pointer.

--- arraypoin3.c
+++ arraypoin3.c
@@ -5,6 +5,6 @@ int main()
 int arr[]={10,20,30,40,50};
 int data[]={100,200,300,400,500},*p;
 p=arr;//Assigns the base address of arr in pointer p.
-printf("%d %u",*p,p);
-getch();
+printf("%u %u\n",&p,arr);
+printf("%d\n",(*(p+2)*10));
 }
