b67ecf21fb6bace0cfe225d627be6a30fd9a5a62
Shashank Lodia <lodiashashank@gmail.com>
2025-09-19 09:23:58 +0530

    Program for how array works with pointer.

---
+++ arraypoint.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int arr[]={10,20,30,40,50},*p;
+    printf("%d %d",*arr,arr[0]);//Array is the part of pointer and its name is base address.
+}
