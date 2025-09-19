449295ad0e3ac1bba8d1b546da44665638e608c8
Shashank Lodia <lodiashashank@gmail.com>
2025-09-19 09:31:12 +0530

    Another program for array with pointer.

---
+++ arraypoin3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int arr[]={10,20,30,40,50};
+int data[]={100,200,300,400,500},*p;
+p=arr;//Assigns the base address of arr in pointer p.
+printf("%d %u",*p,p);
+getch();
+}
